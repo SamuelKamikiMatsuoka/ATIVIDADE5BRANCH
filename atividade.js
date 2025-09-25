ec2fc0c62265604713f7a5391bcf120fff9ea35d
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-09-25 09:21:09 -0300

    alterando e recriando branch bugada

--- atividade.js
+++ atividade.js
@@ -1,65 +1,50 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require("express")();
+const PORT = 2800;
 
+app.get("/calculadora", async (req, res) => {
+  try {
+    const { operacao, numUm, numDois } = req.query;
+    const n1 = parseFloat(numUm);
+    const n2 = parseFloat(numDois);
+    let result;
 
-app.get('/soma/:numUm/:numDois', (req, res) => {
-    const numUm = parseFloat(req.params.numUm);
-    const numDois = parseFloat(req.params.numDois);
-
-    if (isNaN(numUm) || isNaN(numDois)) {
-        return res.status(400).send('Os parâmetros numUm e numDois devem ser números válidos.');
+    if (isNaN(n1) || isNaN(n2)) {
+      return res
+        .status(400)
+        .send(
+          'Erro: Parâmetros "numUm" e "numDois" devem ser números válidos.'
+        );
     }
 
-    const resultado = numUm + numDois;
-    return res.send(`Resultado da soma: ${resultado}`);
+    if (operacao === "soma") {
+      result = n1 + n2;
+    } else if (operacao === "subtracao") {
+      result = n1 - n2;
+    } else if (operacao === "multiplicacao") {
+      result = n1 * n2;
+    } else if (operacao === "divisao") {
+      if (n2 === 0) {
+        return res.status(400).send("Erro: Divisão por zero não é permitida.");
+      }
+      result = n1 / n2;
+    } else {
+      return res
+        .status(400)
+        .send(
+          'Erro: Operação inválida. Use "soma", "subtracao", "multiplicacao" ou "divisao".'
+        );
+    }
+    return res.status(200).send(`O resultado da ${operacao} é: ${result}`);
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  }
 });
 
-
-app.get('/subtrai/:numUm/:numDois', (req, res) => {
-    const numUm = parseFloat(req.params.numUm);
-    const numDois = parseFloat(req.params.numDois);
-
-    if (isNaN(numUm) || isNaN(numDois)) {
-        return res.status(400).send('Os parâmetros numUm e numDois devem ser números válidos.');
-    }
-
-    const resultado = numUm - numDois;
-    return res.send(`Resultado da subtração: ${resultado}`);
+// rota para erro 404 quando a página não for encontrada em relação a URL
+app.use((req, res) => {
+  res.status(404).send("Página não encontrada!");
 });
 
-
-app.get('/multiplica/:numUm/:numDois', (req, res) => {
-    const numUm = parseFloat(req.params.numUm);
-    const numDois = parseFloat(req.params.numDois);
-
-    if (isNaN(numUm) || isNaN(numDois)) {
-        return res.status(400).send('Os parâmetros numUm e numDois devem ser números válidos.');
-    }
-
-    const resultado = numUm * numDois;
-    return res.send(`Resultado da multiplicação: ${resultado}`);
-});
-
-
-app.get('/divide/:numUm/:numDois', (req, res) => {
-    const numUm = parseFloat(req.params.numUm);
-    const numDois = parseFloat(req.params.numDois);
-
-    if (isNaN(numUm) || isNaN(numDois)) {
-        return res.status(400).send('Os parâmetros numUm e numDois devem ser números válidos.');
-    }
-
-    if (numDois === 0) {
-        return res.status(400).send('Erro: Não é possível dividir por zero.');
-    }
-
-    const resultado = numUm / numDois;
-    return res.send(`Resultado da divisão: ${resultado}`);
-});
-
-
-
-app.listen(port, () => {
-    console.log(`Servidor a escutar na porta ${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor executando em localhost na porta ${PORT}`);
 });
