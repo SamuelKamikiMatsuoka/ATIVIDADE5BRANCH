094802e1a475a2f2f896614a8d10c9b0ddc5c27e
SamuelKamikiMatsuoka <samuelmatsuokakamiki@gmail.com>
2025-09-25 09:00:40 -0300

    atualizando atividade6 peso

--- atividade.js
+++ atividade.js
@@ -9,34 +9,52 @@ const PORT = 3000;
 // IMC = peso (kg) / (altura (m) x altura (m))
 
 
-app.get('/imc', (req, res) => {
-    const peso = parseFloat(req.query.peso);
-    const altura = parseFloat(req.query.altura);
-
-    if (isNaN(peso) || isNaN(altura) || altura <=0){
-        return res.status(400).send({erro: `a altura e o peso devem ser maior que zero!`})
-}
-  
-
-
-const imc = peso / (altura*altura); 
-
-
 //Classificação do IMC: APENAS MASSA CORPORAL
 //Baixo peso: Menos de 18,5 kg/m².
 //Peso normal: Entre 18,5 e 24,9 kg/m².
 //Sobrepeso: Entre 25 e 29,9 kg/m².
 //Obesidade: A partir de 30 kg/m².
 
- let classificacao;
-  if (imc < 18.5) {
-    classificacao = 'Baixo peso';
-  } else if (imc >= 18.5 && imc < 25) {
-    classificacao = 'Peso normal';
-  } else if (imc >= 25 && imc < 30) {
-    classificacao = 'Sobrepeso';
-  } else if (imc >= 30 && imc > 30){
-    classificacao = 'Obesidade';
-  }
 
-return res.send(${imc} tofixed(2))
+
+
+async function calculoIMC(peso, altura) {
+  const imc = peso / (altura * altura);
+  try {
+
+    if (imc < 18.5) {
+      return `${imc} Baixo peso`;
+    } else if (imc >= 18.5 && imc < 25) {
+      return 'Peso normal';
+    } else if (imc >= 25 && imc < 30) {
+      return 'Sobrepeso';
+    } else if (imc >= 30 && imc > 30) {
+      return 'Obesidade';
+    }
+  } catch (error) {
+    throw new error(`parametros estão invalidos! Informe numeros ${error}`)
+  }
+}
+
+app.get('/imc/', async (req, res) => {
+  try {
+    const { peso, altura } = req.query;
+    const resultado = await calculoIMC(peso, altura)
+    return res.send(resultado)
+  } catch (error) {
+    res.status(500)({ erro: error.message });
+  }
+})
+
+app.use((req, res) => {
+  res.status(400).send(`Página não encontrada!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor executando em localhost na porta ${PORT}`);
+})
+
+
+
+
+
